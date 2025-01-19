e1795f6a63011d3755830a3853de4d6fbc27e244
Abdurahmon <rahmlow.1217@mail.ru>
2025-01-19 08:37:22 +0500

    adds16files

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+const navAnim = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+
+  burger.addEventListener("click", () => {
+      nav.classList.toggle("nav-active");
+
+
+      navLinks.forEach((link, index) => {
+          if (link.style.animation) {
+              link.style.animation = "";
+          } else {
+              link.style.animation = `navLinkMove 0.5s ease forwards ${index / 7 + 0.35
+              }s`;
+          }
+      });
+
+      burger.classList.toggle("toggle");
+  });  
+};
+
+navAnim();

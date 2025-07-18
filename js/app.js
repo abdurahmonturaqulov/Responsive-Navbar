17747624a067fd4cc9edd3215a9c420e4675547d
Abdurahmon <rahmlow.1217@mail.ru>
2025-07-18 15:54:14 +0500

    2fle add

--- js/app.js
+++ js/app.js
@@ -1,25 +1,24 @@
 
 const navAnim = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
+  const burger = document.querySelector('.burger')
+  const nav = document.querySelector('.nav-links')
+  const navLinks = document.querySelectorAll('.nav-links li')
 
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active')
 
-  burger.addEventListener("click", () => {
-      nav.classList.toggle("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = ''
+      } else {
+        link.style.animation = `navLinkMove 0.5s ease forwards ${
+          index / 7 + 0.35
+        }s`
+      }
+    })
 
+    burger.classList.toggle('toggle')
+  })
+}
 
-      navLinks.forEach((link, index) => {
-          if (link.style.animation) {
-              link.style.animation = "";
-          } else {
-              link.style.animation = `navLinkMove 0.5s ease forwards ${index / 7 + 0.35
-              }s`;
-          }
-      });
-
-      burger.classList.toggle("toggle");
-  });  
-};
-
-navAnim();
+navAnim()
